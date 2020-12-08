f32501a14fe0ca5220867862bb52c9d1b3f640b6
Christian Stark <cstark@cstark.muc.csb>
2020-12-08 12:38:05 +0100

    initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FILES=policies/*
+for f in $FILES
+do
+  if (! grep -q notenforce "$f")
+   then
+     echo "replace inform with enforce in" $f
+     sed -i "s/enforce/inform/g" $f
+  else
+     echo "not replace inform with enforce in" $f
+     sed -i "s/inform/enforce/g" $f
+  fi
+done
